7d4a997b33333ae9222c9c0e82589fa246c52ca2
adi <adi@Adis-MacBook-Air.local>
2025-12-03 19:01:50 -0600

    more fun stuff like tuning

---
+++ MeepMeep/src/main/java/com/example/meepmeep/FieldConstants.java
@@ -0,0 +1,75 @@
+package com.example.meepmeep;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+
+public interface FieldConstants {
+
+
+    double intake = 0;
+    double shoot = 2;
+
+    //-------------------------------Angles------------------------------------------
+
+    //RED
+    double RED_CLOSE_ANGLE = Math.toRadians(-90);
+    double RED_FAR_ANGLE = Math.toRadians(90);
+    double RED_ARTIFACT_ANGLE = Math.toRadians(270);
+
+    //BLUE
+    double BLUE_CLOSE_ANGLE = Math.toRadians(90);
+    double BLUE_FAR_ANGLE = Math.toRadians(0);
+    double BLUE_ARTIFACT_ANGLE = Math.toRadians(90);
+
+    //-------------------------------Velocities------------------------------------------
+    int CLOSE_VELOCITY = 1770;
+    int FAR_VELOCITY = 2500;
+
+    //-------------------------------Coordinates------------------------------------------
+
+    // ======Artifact Length======
+    double ARTIFACT_DIST = 36+5;
+    double HP_ARTIFACT_DIST = 62;
+    // ======RED ARTIFACTS======
+//    Vector2d GPP_RED_ARTIFACT = new Vector2d(-36, -25);
+//    Vector2d PGP_RED_ARTIFACT = new Vector2d(-12, -25);
+//    Vector2d PPG_RED_ARTIFACT = new Vector2d(12, -54);
+
+    Vector2d GPP_RED_ARTIFACT = new Vector2d(-36, -24);
+    Vector2d PGP_RED_ARTIFACT = new Vector2d(-12, -24);
+    Vector2d PPG_RED_ARTIFACT = new Vector2d(3, -56);
+    Vector2d HP_RED_ARTIFACT = new Vector2d(-60,-58);
+
+
+    // ======RED Shooting Locations======
+    Vector2d RED_CLOSE_SHOOT = new Vector2d(12, -10);
+    Pose2d RED_FAR_SHOOT = new Pose2d(-61.25, -5, Math.toRadians(-90));
+
+    // ======BLUE ARTIFACTS======
+    Vector2d GPP_BLUE_ARTIFACT = new Vector2d(-36, 24);
+    Vector2d PGP_BLUE_ARTIFACT = new Vector2d(-12, 24);
+    Vector2d PPG_BLUE_ARTIFACT = new Vector2d(3, 56);
+    Vector2d HP_BLUE_ARTIFACT = new Vector2d(-60,58);
+
+
+    // ======Blue Shooting Locations======
+    Vector2d BLUE_CLOSE_SHOOT = new Vector2d(5,7);
+    Pose2d BLUE_FAR_SHOOT = new Pose2d(-61.25, 5, Math.toRadians(0));
+
+
+    // ======Gates======
+    Vector2d BLUE_GATE = new Vector2d(-12, 66);
+    Vector2d RED_GATE = new Vector2d(-12, -66);
+
+    // ======STARTING POSITIONS======
+    Pose2d RED_FAR_START = new Pose2d(-61.25, -5, Math.toRadians(0));
+
+    Pose2d RED_CLOSE_START = new Pose2d(57, -45, Math.toRadians(-143));
+
+    Pose2d BLUE_FAR_START = new Pose2d(-61.25, 5, Math.toRadians(0));
+
+    Pose2d BLUE_CLOSE_START = new Pose2d(57, 45, Math.toRadians(323));
+
+    Pose2d BLUE_GOAL_START = new Pose2d(-47.5, 51.5,  Math.toRadians(323));
+
+}
---
+++ MeepMeep/src/main/java/com/example/meepmeep/RED_CLOSE.java
@@ -0,0 +1,208 @@
+package com.example.meepmeep;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+public class RED_CLOSE {
+
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+            .setConstraints(70, 70, 2.5, 3, 18)
+            .setDimensions(16.53, 18)
+            .build();
+
+        Action preload = myBot.getDrive().actionBuilder(FieldConstants.RED_CLOSE_START)
+            .strafeToLinearHeading(FieldConstants.RED_CLOSE_SHOOT, FieldConstants.RED_CLOSE_ANGLE)
+            .build();
+
+        Action artifact1 = myBot.getDrive().actionBuilder(new Pose2d(FieldConstants.RED_CLOSE_SHOOT.x, FieldConstants.RED_CLOSE_SHOOT.y, FieldConstants.RED_CLOSE_ANGLE))
+            .strafeToLinearHeading(FieldConstants.PPG_RED_ARTIFACT, FieldConstants.RED_ARTIFACT_ANGLE)
+
+            .build();
+
+        Action artifact1_return = myBot.getDrive().actionBuilder(new Pose2d(FieldConstants.PPG_RED_ARTIFACT.x, FieldConstants.PPG_RED_ARTIFACT.y, FieldConstants.RED_ARTIFACT_ANGLE))
+
+            .strafeToLinearHeading(FieldConstants.RED_CLOSE_SHOOT, FieldConstants.RED_CLOSE_ANGLE)
+            .waitSeconds(0.85)
+
+            .build();
+
+
+        Action artifact2 = myBot.getDrive().actionBuilder(new Pose2d(FieldConstants.RED_CLOSE_SHOOT.x, FieldConstants.RED_CLOSE_SHOOT.y, FieldConstants.RED_CLOSE_ANGLE))
+            .strafeToLinearHeading(FieldConstants.PGP_RED_ARTIFACT, FieldConstants.RED_ARTIFACT_ANGLE)
+
+            .setTangent(FieldConstants.RED_ARTIFACT_ANGLE)
+            //
+            .lineToY(FieldConstants.PGP_RED_ARTIFACT.y+FieldConstants.ARTIFACT_DIST-10)
+
+            .build();
+
+        Action artifact2_return = myBot.getDrive().actionBuilder(new Pose2d(FieldConstants.PGP_RED_ARTIFACT.x, FieldConstants.PGP_RED_ARTIFACT.y+FieldConstants.ARTIFACT_DIST- 10, FieldConstants.RED_ARTIFACT_ANGLE))
+
+            .strafeTo(FieldConstants.PGP_RED_ARTIFACT)
+            .strafeToLinearHeading(FieldConstants.RED_CLOSE_SHOOT, FieldConstants.RED_CLOSE_ANGLE+Math.toRadians(5-2))
+
+//                            .splineToLinearHeading(new Pose2d(FieldConstants.RED_CLOSE_SHOOT, FieldConstants.RED_CLOSE_ANGLE))
+            .setReversed(true)
+            .splineToConstantHeading(FieldConstants.RED_CLOSE_SHOOT, 0)
+
+            .build();
+
+
+
+        Action artifact3 = myBot.getDrive().actionBuilder(new Pose2d(FieldConstants.RED_CLOSE_SHOOT.x, FieldConstants.RED_CLOSE_SHOOT.y, FieldConstants.RED_CLOSE_ANGLE))
+            //                .strafeToLinearHeading(GPP_RED_ARTIFACT, RED_ARTIFACT_ANGLE)
+
+            .setTangent(Math.PI)
+            .splineToConstantHeading(FieldConstants.GPP_RED_ARTIFACT, Math.PI/2)
+            .waitSeconds(.2)
+            .lineToY(FieldConstants.GPP_RED_ARTIFACT.y+FieldConstants.ARTIFACT_DIST+7)
+
+            .build();
+
+        Action artifact3_return = myBot.getDrive().actionBuilder(new Pose2d(FieldConstants.GPP_RED_ARTIFACT.x, FieldConstants.GPP_RED_ARTIFACT.y+FieldConstants.ARTIFACT_DIST+7, FieldConstants.RED_ARTIFACT_ANGLE))
+
+            //                .setReversed(true)
+            .strafeToLinearHeading(FieldConstants.RED_CLOSE_SHOOT, FieldConstants.RED_CLOSE_ANGLE)
+
+            .build();
+
+        myBot.runAction(
+            new SequentialAction(
+                preload,
+                artifact1,
+//                botActions.preload_parallel_blue(preload),
+//
+//                botActions.shoot_parallel(),
+//
+//                robot.outtake.stopAction(),
+//
+//
+//
+//
+//
+//
+//
+//                botActions.intake_parallel(artifact1),
+//
+//                robot.intake.stop(),
+
+                new ParallelAction(
+                    artifact1_return
+                    //                                subsystems.intake.intakeReverse(0.5),
+
+//                    new SequentialAction(
+//
+////                        robot.outtake.reverseAction(.5),
+////                        robot.outtake.shootVelocityAction(CLOSE_VELOCITY)
+//
+//                    )
+                ),
+//                robot.outtake.stopAction(),
+
+
+//                new SequentialAction(
+//                    //                                subsystems.outtake.shoot_close(),
+//                    new ParallelAction(
+////                        robot.outtake.shootVelocityTimeAction(CLOSE_VELOCITY, SHOOTER_TIME),
+////                        robot.intake.intakeTimeAction(SHOOTER_TIME)
+//                    )
+//
+//                ),
+
+//                robot.outtake.stopAction(),
+
+
+
+
+
+                // ARTIFACT 2
+
+                new ParallelAction(
+                    artifact2
+//                    robot.intake.intakeTimeAction(INTAKE_WAIT_TIME),
+//                    robot.outtake.shootVelocityTimeAction(CLOSE_VELOCITY, INTAKE_WAIT_TIME)
+                ),
+//                robot.intake.stop(),
+
+                new ParallelAction(
+                    artifact2_return
+                    //                                subsystems.intake.intakeReverse(0.5),
+
+//                    new SequentialAction(
+//
+////                        robot.outtake.reverseTimeAction(.5),
+////                        robot.outtake.shootVelocityAction(CLOSE_VELOCITY)
+//
+//                    )
+                ),
+
+//                new SequentialAction(
+//                    //                                subsystems.outtake.shoot_close(),
+//                    new ParallelAction(
+////                        robot.outtake.shootVelocityTimeAction(CLOSE_VELOCITY, SHOOTER_TIME),
+////                        robot.intake.intakeTimeAction(SHOOTER_TIME)
+//                    )
+//
+//                ),
+//                robot.outtake.stopAction(),
+
+
+
+
+
+                // ARTIFACT 3
+
+                new ParallelAction(
+                    artifact3
+//                    robot.intake.intakeTimeAction(INTAKE_WAIT_TIME-.5),
+//                    robot.outtake.reverseTimeAction(INTAKE_WAIT_TIME-.5)
+                ),
+//                robot.intake.stop(),
+
+                new ParallelAction(
+                    artifact3_return
+                    //                                subsystems.intake.intakeReverse(0.5),
+
+//                    new SequentialAction(
+//                        robot.outtake.shootVelocityAction(CLOSE_VELOCITY)
+//
+//                    )
+                )
+
+//                new SequentialAction(
+//                    robot.intake.intakeTimeAction(SHOOTER_TIME)
+//                )
+            )
+
+        );
+
+        BufferedImage bg = null;
+        try {
+            bg = ImageIO.read(new File("MeepMeep/src/main/java/com/example/meepmeep/DECODE.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        meepMeep.setBackground(bg)
+            .setDarkMode(true)
+            .setBackgroundAlpha(0.95f)
+            .addEntity(myBot)
+            .start();
+
+
+    }
+}
