f45bc4db47a0d2123f4ee33eda5b9b7b2b6aebaa
adi <adi@Adis-MacBook-Air.local>
2025-12-12 19:59:19 -0600

    autovelo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake/Outtake.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
@@ -24,7 +25,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.drive.PoseTransfer.PoseStorage;
 
-
+@Config
 public class Outtake {
     public DcMotorEx motor1;
     public DcMotorEx motor2;
@@ -34,8 +35,10 @@ public class Outtake {
     MultipleTelemetry telemetry;
 
 
+
     public static double P = 500, I = 0, D = 0, F = 14.3;
-//    public static double K = 0.0035; // saturation rate for the hood function, needs to be tuned
+    public static double K = 0.05; // saturation rate for the hood function, needs to be tuned
+    public static double T = 120; // saturation rate for the hood function, needs to be tuned
 //    double MIN_HOOD = 20; // need to determine this, btw these are all in degrees
 //    double MAX_HOOD = 60; // need to determine this
 
@@ -79,7 +82,7 @@ public class Outtake {
     }
 
 
-    public void autoVelocity(double Rx, double Ry) {
+    public void autoVelocity(double R) {
         // calculate hood first
 //        double hoodPos = hoodCalc(Rx, Ry);  // hoodCalc just returns a value
 //        hood.setPosition(hoodPos);          // then apply it to the servo
@@ -87,26 +90,16 @@ public class Outtake {
 
 
         // calculate velocity based on hood
-        int velocity = veloCalc(Rx, Ry, hoodPos);  // use hoodPos as input
-
+        int velocity = veloCalc(R, hoodPos);  // use hoodPos as input
+        setVelocity(velocity);
 
         // apply PID to reach velocity
-//        telemetry.addData("PID Output", pidOutput);
-//        telemetry.addData("Setpoint", velocity);
-//        telemetry.addData("Error", velocity - (motor1.getVelocity()+ motor2.getVelocity())/2);
+        telemetry.addData("Setpoint", velocity);
+        telemetry.addData("Error", velocity - (motor1.getVelocity()+ motor2.getVelocity())/2);
     }
 
 
     public void shootVelocity(int velocity) {
-       /*
-       velocityController.setSetpoint(velocity);
-       pidOutput = velocityController.getOutput(Math.abs(getVelocity()));
-       telemetry.addData("PID Output", pidOutput);
-       telemetry.addData("Setpoint", velocity);
-       telemetry.addData("Error", velocity - (motor1.getVelocity()+ motor2.getVelocity())/2);
-       setPower(pidOutput);
-       */
-
 
         motor1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(P, I, D, F));
         motor2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(P, I, D, F));
@@ -121,9 +114,12 @@ public class Outtake {
         motor1.setPower(0);
         motor2.setPower(0);
     }
-    public int veloCalc(double Rx, double Ry, double hoodPos) {
-        int velocity = 1100; /* formula that depends on hoodPos, Rx, Ry */
-        return velocity;
+    public int veloCalc(double R, double hoodPos) {
+//        int velocity = 1100; /* formula that depends on hoodPos, Rx, Ry */
+//        double velocity = 1000 + R * K;
+        double velocity = 1500 - 400 / (1 + Math.exp(K * (R - T)));
+        telemetry.addData("Vel", 400 / (1 + Math.exp(K * (R - T))));
+        return (int) velocity;
     }
 //    public double hoodCalc(double Rx, double Ry) {
 //        double GOAL_X = 72;
