475878f9e17e8d3afbe17962dd67f71603d41f04
adi <adi@Adis-MacBook-Air.local>
2026-01-04 21:59:07 -0600

    oops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MecanumDrive.java
@@ -240,8 +240,8 @@ public final class MecanumDrive {
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // TODO: `reverse` motor directions if needed
-        ;
-        //rightBack.setDirection(DcMotorEx.Direction.REVERSE);
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBack.setDirection(DcMotorEx.Direction.REVERSE);
 
         // TODO: make sure your config has an IMU with this name (can be BNO or BHI)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
